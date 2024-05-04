0a3c910624a9f8d960dccb05f128e68c2e394ac7
edward.nguyen <edward.nguyen@merkle.com>
2024-05-05 01:30:36 +1000

    fix error string name lambda function

--- lib/lambdastack.ts
+++ lib/lambdastack.ts
@@ -17,7 +17,7 @@ export class LambdaStack extends Stack {
 
         // Define a new Lambda function with Python runtime
         const exampleLambda = new lambda.Function(this, 'lambda_function', {
-            functionName: 'lambda_function_{props.stageName}',  // Function name will be 'lambda_function_dev' or 'lambda_function_prod
+            functionName: 'lambda_function_${props.stageName}',  // Function name will be 'lambda_function_dev' or 'lambda_function_prod
             code: lambda.Code.fromAsset(lambdaCodePath),  // Adjust the path to your Python code
             handler: 'lambda_function.lambda_handler',  // Assumes your Python handler is named 'lambda_function.py' with a function 'lambda_handler'
             runtime: lambda.Runtime.PYTHON_3_9,  // Python 3.9 runtime
