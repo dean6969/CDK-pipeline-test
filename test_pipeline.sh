e65cff07108043eb40b3da61ac36e87ab5ab55ab
edward.nguyen <edward.nguyen@merkle.com>
2024-05-05 04:48:38 +1000

    hahajdsadasddfdf0434dsdsdsadadsa431432dasdjlks

--- test_pipeline.sh
+++ test_pipeline.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-set -e  # Exit immediately if a command exits with a non-zero status.
-set -x  # Debug mode, which prints each command before executing it.
-
 # Create a Python virtual environment
 python -m venv venv
 
@@ -16,8 +12,11 @@ py_chk="$(pycodestyle --max-line-length=120 */*/*.py)"
 echo "$py_chk"
 
 # Check if pycodestyle output is not empty (which means style errors were found)
-if [ -n "$py_chk" ]; then  
-  exit 1;
+py_chk=\"$(pycodestyle --max-line-length=120 */*/*.py)\"
+echo $py_chk
+if [ ${#py_chk} != 0 ]; 
+then  
+exit 1;
 fi
 
 # Deactivate the virtual environment
