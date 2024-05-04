ff173bd2e82c19813af235ad6659e4d84fbb492d
edward.nguyen <edward.nguyen@merkle.com>
2024-05-05 04:30:38 +1000

    hahajdsadasddfdf0434dsdsdsadadsadasds

--- lib/module/lambda_function.py
+++ lib/module/lambda_function.py
@@ -3,7 +3,7 @@ import json
 def lambda_handler(event, context):
     # Print the event to the logs of the Lambda function, which can be viewed in CloudWatch
     print("Received event: " + json.dumps(event, indent=2))
-    
+
     # Respond back with a simple message and the received event
     return {
         'statusCode': 200,
